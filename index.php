287b430fed6eb926fa07ee21e2ed5501118b9eaa
pyroalternate <90666984+pyroalternate@users.noreply.github.com>
2022-03-15 21:13:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,91 +1,83 @@
-<!DOCTYPE html> 
-<section id="page">
-  <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
-    <script src="/js/all.js"></script>
-     <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-2402LSMNNG"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-  
-        gtag('config', 'G-2402LSMNNG');                              
-      </script>
-  
-    <script src="/js/changeSrc.js"></script>
-  
-  
-  
-  
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4725442492639064"
-       crossorigin="anonymous"></script>
-  
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="a new internet.">
-      <meta name="author" content="astro">
-      <meta property="og:image" content="/img/logo.png">
-      <title>astro</title>
-      <link rel="shortcut icon" type="image/png" href="./index_files/logo.svg">
-      <link rel="stylesheet" href="./index_files/style.css">
-      <link rel="stylesheet" href="./index_files/light.css">
-      <link rel="stylesheet" href="https://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
-      <script type="text/javascript" src="https://code.jquery.com/jquery-1.6.4.min.js"></script>
-      <script type="text/javascript" src="https://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
-      <link rel="stylesheet" href="./index_files/all.min.css">
-      <script src="./index_files/jquery.min.js.download"></script>
-      <script src="./index_files/index.js.download"></script>
-  
-    
-      <script src="./index_files/go.js.download"></script>
-      
-  </head>
-  
-  <div data-role="page" data-theme="a" class="container" id="one">
-  <body appearance="dark" class="homepage">
-  
-  
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <script src="/js/all.js"></script>
+   <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2402LSMNNG"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-  <div class="navbar">
+      gtag('config', 'G-2402LSMNNG');                              
+    </script>
+
+  <script src="/js/changeSrc.js"></script>
+
+
+
+
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4725442492639064"
+     crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="a new internet.">
+    <meta name="author" content="astro">
+    <meta property="og:image" content="/img/logo.png">
+    <title>astro</title>
+    <link rel="shortcut icon" type="image/png" href="./index_files/logo.svg">
+    <link rel="stylesheet" href="./index_files/style.css">
+    <link rel="stylesheet" href="./index_files/light.css">
+    <link rel="stylesheet" href="./index_files/all.min.css">
+    <script src="./index_files/jquery.min.js.download"></script>
+    <script src="./index_files/index.js.download"></script>
+
   
-  <div class="navname">astro</div>
-   
-  <link rel="stylesheet" href="./index_files/all.min(1).css">
-  
-  <div class="navitem arcade"><a href="#one">Home</a></div>
-  <div class="navitem chatbox"><a href="/index5">astroFlix</a></div><div class="navitem settings"><a href="/index3">astroCade</a></div><div class="navitem cloaking"><a href="/index4">Cloak</a></div>
-  
-  
-  </div>
-  
-  <img class="homelogo" height="200px" src="./index_files/logo.svg">
-  
-  <p class="homename">astro</p>
-  
-  <center>
-  <i class="fas fa-search searchicon"></i>
-  <input autofocus="" autocomplete="off" class="search" id="search" placeholder="Type URL">
-  
-  <div class="proxies">
-  
-  <div onclick="setproxy(&#39;Corrosion&#39;)" class="proxy" id="corrosion">Palladium</div>
-  
-  <div onclick="setproxy(&#39;Smoke&#39;)" class="proxy" id="smoke">Rhodium</div></div>
-  </center>
-  
-  <iframe width="100%" height="732px" src="./index_files/saved_resource.html" id="surf"></iframe>
-  <div id="closesurf" onclick="closesurf()"><i id="closeicon" class="fas fa-times"></i></div>
-  <div id="reloadsurf" onclick="reloadsurf()"><i id="reloadicon" class="fas fa-redo-alt"></i></div>
-  
-  
-  
-  
-  </div>
-  </div>
-  
-  </div>
-  </div>
-  
-  <div class="footer">astro | <a class="link" href="https://discord.gg/uncensor">Discord</a> </div>
-</section>
+    <script src="./index_files/go.js.download"></script>
+    
+</head>
+<body appearance="dark" class="homepage">
+
+
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+<div class="navbar">
+
+<div class="navname">astro</div>
+ 
+<link rel="stylesheet" href="./index_files/all.min(1).css">
+
+<div class="navitem arcade"><a href="/">Home</a></div>
+<div class="navitem chatbox"><a href="/index5">astroFlix</a></div><div class="navitem settings"><a href="/index3">astroCade</a></div><div class="navitem cloaking"><a href="/index4">Cloak</a></div>
+
+
+</div>
+
+<img class="homelogo" height="200px" src="./index_files/logo.svg">
+
+<p class="homename">astro</p>
+
+<center>
+<i class="fas fa-search searchicon"></i>
+<input autofocus="" autocomplete="off" class="search" id="search" placeholder="Type URL">
+
+<div class="proxies">
+
+<div onclick="setproxy(&#39;Corrosion&#39;)" class="proxy" id="corrosion">Palladium</div>
+
+<div onclick="setproxy(&#39;Smoke&#39;)" class="proxy" id="smoke">Rhodium</div></div>
+</center>
+
+<iframe width="100%" height="732px" src="./index_files/saved_resource.html" id="surf"></iframe>
+<div id="closesurf" onclick="closesurf()"><i id="closeicon" class="fas fa-times"></i></div>
+<div id="reloadsurf" onclick="reloadsurf()"><i id="reloadicon" class="fas fa-redo-alt"></i></div>
+
+
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+<div class="footer">astro | <a class="link" href="https://discord.gg/uncensor">Discord</a> </div>
