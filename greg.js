b2c5d22bee9722eb93e644f8695b97a4e203186e
pyroalternate <90666984+pyroalternate@users.noreply.github.com>
2022-04-01 15:12:31 -0500

    Create greg.js

---
+++ greg.js
@@ -0,0 +1,11 @@
+var password;
+var pass1="greglikesmen";
+
+password=prompt('Please enter your password to view this page!',' ');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    }
